15f365cf77774df0efb001d8600bfd3eedd2d96c
Jared Knowles <jknowles@gmail.com>
2014-09-07 12:39:33 -0500

    group effects for merMod

--- pkg/tests/merMod.R
+++ pkg/tests/merMod.R
@@ -11,15 +11,33 @@ apsrtable(fm1, lev = 0)
 
 (fm3 <- lmer(Reaction ~ 0 + (1 | Subject) + (1 | Days), sleepstudy))
 
-apsrtable(fm1, fm2, lev = 0)
+(fm3 <- lmer(Reaction ~ 0 + (1 | Subject) + (1 | Days), sleepstudy))
 
-summod <- summary(fm3)
+sleepstudy$Noise <- rnorm(nrow(sleepstudy))
+sleepstudy$Wave <- sample(c("A", "B", "C"), nrow(sleepstudy), replace = TRUE)
+
+
+(fm4 <- lmer(Reaction ~ 0 + (1 | Subject) + (Noise | Days) + (1|Wave), sleepstudy))
+
+apsrtable(fm1, fm2, fm3, fm4, lev = 0)
+
+summod <- summary(fm4)
 
 GroupList <- vector("list", length(summod$varcor) * 2)
 nams <- paste0("Group:", names(summod$varcor))
-names(GroupList) <- paste0(rep(nams, 2), rep(c(" Effs.", " Var."), 
-                                             length(summod$varcor)))
+names(GroupList) <- paste0(rep(nams, times = 2), rep(c(" Effs.", " Var."), 
+                                             each = length(summod$varcor)))
 for(i in 1:length(summod$varcor)){
-  GroupList[[i]] <- 
-  
+  GroupList[[i]] <- paste(names(attr(summod$varcor[[i]], "stddev")), " | ")
+  GroupList[[i + length(summod$varcor)]] <- paste(round(attr(summod$varcor[[i]], "stddev"), 
+                                     digits = 3), collapse = " | ")
 }
+GroupList["Sigma"] <- attr(summod$varcor, "sc")
+
+
+model.info <- list(
+  "$N$"=formatC(as.numeric(summod$devcomp$dims['n']),format="d"),
+  "AIC"=formatC(as.numeric(summod$AICtab),
+                format="f",digits=3),
+  "N Groups"=as.numeric(summod$ngrps), 
+  "Group Names" = names(summod$varcor))
