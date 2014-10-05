36126aae1a12419ea498b2f44b2d9d75e09ee44c
Jared Knowles <jknowles@gmail.com>
2014-10-05 14:07:22 -0500

    fix stars for lmer objects

--- pkg/R/apsrtable.R
+++ pkg/R/apsrtable.R
@@ -350,9 +350,19 @@ apsrtable <- function (...,
         model.out <- model.se.out <- star.out <- rep(NA,length(coefnames))
         model.out[var.pos] <- s$coefficients[,1]
         if(lev>0) {
+          if("t value" %in% colnames(s$coefficients)){
+            message("Using t-test with df = 100 to calculate stars.")
+            nctmp <- ncol(s$coefficients)
+            s$coefficients <- cbind(s$coefficients, dt(s$coefficients[, nctmp], 100))
+            colnames(s$coefficients)[nctmp+1] <- "Pr(>|z|)"
             star.out[var.pos] <- apsrStars(s$coefficients,
                                            stars=stars,
                                            lev=lev,signif.stars=TRUE)
+          } else{
+            star.out[var.pos] <- apsrStars(s$coefficients,
+                                           stars=stars,
+                                           lev=lev,signif.stars=TRUE)
+          }
         } else {
             star.out <- rep("", length(coefnames))
         }
